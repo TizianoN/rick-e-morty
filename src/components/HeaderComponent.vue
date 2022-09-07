a446e883a7987dbfdfb86e9c12d6e81b547bfba9
Tiziano <hello@tizianonicolai.com>
2022-09-07 10:36:53 +0200

    scaffolding styles + font install

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hello">
+        <h1 class="main-title my-5">{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderComponent",
+    props: {
+        msg: String,
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../styles/general.scss";
+</style>
